c3dc73b7062be30d3a1a1ea8f8d57c5f12d37ec9
cherrycider <korniychuk@i.ua>
2018-06-05 14:26:48 +0300

    pg connection

--- wiknot-web/db_connection.php
+++ wiknot-web/db_connection.php
@@ -7,29 +7,29 @@
 
 // PG database : - extend the app to add a PDO connection
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
+//$dbopts = parse_url(getenv('DATABASE_URL'));
+//$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+//               array(
+//                'pdo.server' => array(
+//                   'driver'   => 'pgsql',
+//                   'user' => $dbopts["user"],
+//                   'password' => $dbopts["pass"],
+//                   'host' => $dbopts["host"],
+//                   'port' => $dbopts["port"],
+//                   'dbname' => ltrim($dbopts["path"],'/')
+//                   )
+//               )
+//);
 
-$db = pg_connect ("
-			host=$dbopts["host"] 
-			port=$dbopts["port"] 
-			dbname=ltrim($dbopts["path"] ,'/')
-			user=$dbopts["user"] 
-			password=$dbopts["pass"]
-		");
+//$db = pg_connect ("
+//			host=$dbopts["host"] 
+//			port=$dbopts["port"] 
+//			dbname=ltrim($dbopts["path"] ,'/')
+//			user=$dbopts["user"] 
+//			password=$dbopts["pass"]
+//		");
 
 //most simple line:
-//$db = pg_connect (getenv("DATABASE_URL"));
+$db = pg_connect (getenv("DATABASE_URL"));
 
 ?>
